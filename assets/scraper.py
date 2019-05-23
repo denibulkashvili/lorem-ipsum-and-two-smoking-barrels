3b1018ab22a34d5b7a1e2a352ab3191623b7c4c2
denibulkashvili <denibulkashvili@gmail.com>
2019-05-24 01:13:37 +0700

    Refactor scraper and update logic

--- assets/scraper.py
+++ assets/scraper.py
@@ -3,29 +3,56 @@ import json
 from bs4 import BeautifulSoup
 import requests
 
-#pylint:disable=invalid-name
 # get the source
-website_url = 'https://www.rottentomatoes.com/m/lock_stock_and_two_smoking_barrels/quotes/'
-source = requests.get(website_url).text
-soup = BeautifulSoup(source, 'lxml')
+MOVIES = [
+    {
+        'name': 'LOCK',
+        'url': 'https://www.rottentomatoes.com/m/snatch/quotes/'
+    },
+    {
+        'name': 'SNATCH',
+        'url': 'https://www.rottentomatoes.com/m/lock_stock_and_two_smoking_barrels/quotes/'
+    }
+]
 
-# get all quotes as a list of <li> elements
-quotes = soup.find_all('div', class_="quoteBody")
+class Scraper:
+    """Creates a scraper"""
 
-# create an empty dictionary of quotes
-data = []
+    def __init__(self, website_url, movie_name):
+        self.website_url = website_url
+        self.source = requests.get(self.website_url).text
+        self.soup = BeautifulSoup(self.source, 'lxml')
+        self.movie_name = movie_name
 
-for quote in quotes:
-    movie = "LOCK"
-    # get the two parts (actor and text) and combine
-    content = quote.find_all("span")
-    text = f"{content[0].text} {content[1].text}"
+    def find_quotes(self):
+        """get all quotes as a list of <li> elements"""
+        quotes = self.soup.find_all('div', class_="quoteBody")
+        return quotes
 
-    # append quote text to the dictionary
-    data.append({
-        "movie" : movie,
-        "text" : text
-    })
+    def save_quotes_list(self):
+        """Get the quotes form the soup"""
+        quotes_list = []
+        quotes = self.find_quotes()
 
-with open('assets/lock_quotes.json', 'w') as outfile:
-    json.dump(data, outfile)
+        for quote in quotes:
+            content = quote.get_text()
+            quotes_list.append({
+                "model": "quotes.Quote",
+                "fields": {
+                    "movie" : self.movie_name,
+                    "text" : content,
+                    "quote_len": len(content)
+                }
+            })
+
+        return quotes_list
+
+
+if __name__ == "__main__":
+    for movie in MOVIES:
+
+        s = Scraper(movie["url"], movie["name"])
+        data = s.save_quotes_list()
+
+        with open(f'quotes/fixtures/{movie["name"]}.json', 'w') as outfile:
+            json.dump(data, outfile)
