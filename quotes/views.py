573097e3351b1e58d8d417a631217a755ca99526
denibulkashvili <denibulkashvili@gmail.com>
2019-05-29 11:56:24 +0700

    Pass json object to view

--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,5 @@
 """Quotes views"""
+import json
 from django.views.generic import ListView
 from .models import Quote
 
@@ -7,3 +8,19 @@ class HomeView(ListView):
     """Home page view"""
     model = Quote
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        """Pass quotes as json objects to context"""
+        context = super().get_context_data(**kwargs)
+        quotes_list = []
+        for quote in Quote.objects.all():
+            new_quote = {
+                'id': quote.id,
+                'text': quote.text,
+                'movie': quote.movie,
+                'quote_len': quote.quote_len
+            }
+            quotes_list.append(new_quote)
+        context["quotes_list"] = json.dumps(quotes_list)
+        return context
+   
